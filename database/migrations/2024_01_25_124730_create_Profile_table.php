b6d161ed77ca22cd51e5978695b5f9d9f473b6b2
heritier24 <heritier.ganza@kudibooks.com>
2024-01-31 13:04:40 +0200

    profile improved details of migrations

--- database/migrations/2024_01_25_124730_create_Profile_table.php
+++ database/migrations/2024_01_25_124730_create_Profile_table.php
@@ -13,19 +13,23 @@ return new class extends Migration
     {
         Schema::create('Profile', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger("user_id");
+            $table->unsignedBigInteger("linka_user_id");
             $table->string("firstName");
             $table->string("lastName");
+            $table->string("nickName")->comment("This nick name will take name of username from users");
             $table->string("age");
             $table->string("gender");
             $table->string("country");
             $table->string("height");
             $table->string("weight");
-            $table->string("personalInfo");
+            $table->text("personalInfo");
+            $table->string("sexualOrientation")->comment("this could be Straight, Gay, Lesbian, bisexual, asexual, demi sexual");
+            $table->string("lookingFor")->default("Relationship")->comment("Here could be serious or just friendship");
+            $table->text("lookingDescription");
             $table->string("profileImagePath");
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users')
+            $table->foreign('linka_user_id')->references('id')->on('LinkaUsers')
                 ->onDelete('restrict')
                 ->onUpdate('cascade');
         });
