d0bfabaf6197769d4dfa623057fe8d9103ab2a40
heritier24 <heritier.ganza@kudibooks.com>
2024-02-07 12:23:45 +0200

    reggister and login with google email method created well

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Models\LinkaUsers;
+use App\Models\User;
 use App\Services\AuthenticationService;
+use Illuminate\Support\Facades\DB;
+use Laravel\Socialite\Facades\Socialite;
 
 class AuthenticationController extends Controller
 {
@@ -39,5 +43,72 @@ class AuthenticationController extends Controller
                 "message" => $message,
             ], $statusCode);
         }
+    }
+
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver("google")->user();
+
+        // Check if the user already exists in the database
+        $existingUser = User::where('email', $user->email)->first();
+
+        if ($existingUser) {
+
+            $linkaUserID = $this->validateLinkaUser($existingUser->id);
+
+            $token = $user->token;
+
+            $response = [
+                "userID" => $linkaUserID,
+                "name" => $user->name,
+                "googleProfile" => $user->avatar,
+                "token" => $token,
+                "message" => "Login Successfully",
+            ];
+        } else {
+            // Create a new user with the received data
+            $newUser = User::create([
+                'name' => $user->name,
+                'email' => $user->email,
+                // Add other relevant fields
+            ]);
+
+            LinkaUsers::create([
+                "user_id" => $newUser->id,
+                "user_type_id" => 1, //  this is user type of member user
+                "provider" => "google",
+                "provider_id" => $user->id,
+            ]);
+
+            $linkaUserID = $this->validateLinkaUser($newUser->id);
+
+            $token = $user->token;
+
+            $response = [
+                "userID" => $linkaUserID,
+                "name" => $user->name,
+                "googleProfile" => $user->avatar,
+                "token" => $token,
+                "message" => "Login Successfully",
+            ];
+        }
+
+        return response()->json($response);
+    }
+
+    public function validateLinkaUser(int $userID)
+    {
+        $linkaUserID = DB::selectOne("SELECT LinkaUsers.id FROM LinkaUsers
+                                      INNER JOIN users
+                                      ON LinkaUsers.user_id = users.id
+                                      WHERE users.id = ?", [$userID]);
+
+        return $linkaUserID->id;
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/auth/{provider}', [AuthenticationController::class, 'redirectToProvider']);
+Route::get('/auth/google/callback', [AuthenticationController::class, 'handleProviderCallback']);
