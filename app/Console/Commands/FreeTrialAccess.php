2accda2b8c8c948f4c1f6b1aed2d73122a92fcf3
heritier24 <heritier.ganza@kudibooks.com>
2024-02-08 14:57:50 +0200

    command to register free trial access created

---
+++ app/Console/Commands/FreeTrialAccess.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\LinkaUsers;
+use App\Models\SubscriptionLinka;
+use App\Models\SubscriptionLinkaMembersType;
+use Illuminate\Console\Command;
+
+class FreeTrialAccess extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'free:trial';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This is command willbe used for free trial access to the app chatting';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Get all member users of the linka
+        $linkaMembers = LinkaUsers::all();
+
+        $freeTrialID = SubscriptionLinka::where("packageName", "Free Trial")->first();
+
+        foreach ($linkaMembers as $member) {
+
+            SubscriptionLinkaMembersType::firstOrCreate([
+                "linka_user_id" => $member->id,
+                "subscription_type_linka_id" => $freeTrialID->id,
+                "packageName" => "Free Trial",
+                "amount" => "0",
+                "status" => "Active",
+            ]);
+        }
+
+        $this->info("Free Trial Activated for new member registered");
+    }
+}
