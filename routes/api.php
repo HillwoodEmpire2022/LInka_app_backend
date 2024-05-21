9118512e80c15bba40ca6d882267a663f466f9ac
Jeph2001 <josephmaniragaba9@gmail.com>
2024-05-21 16:44:14 +0200

    deleting unwanted files

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\ChattingController;
-use App\Http\Controllers\MachingController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ConversationController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,23 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->group(function () {
 
 
-});
-
-Route::post('/login', [AuthenticationController::class, 'loginUser']); // working well
-
-Route::post('/register', [AuthenticationController::class, 'registerUser']); // working well
-
-Route::prefix("/profile")->group(function() { 
-
-    Route::post("/create/{userID}", [ProfileController::class, "createProfile"])->whereNumber("userID"); // working well. b ut it is not protected
-
-    Route::put("/update/{userID}", [ProfileController::class, "updateProfile"])->whereNumber("profileID"); // working well. b ut it is not protected
-
-    Route::post("/disable/{profileID}", [ProfileController::class, "disableProfile"])->whereNumber("profileID");
-
-    Route::get("/list", [ProfileController::class, "listProfile"]); // working well. b ut it is not protected
-
-    Route::get("/list/{userID}", [ProfileController::class, "profileDetails"]); // working well. b ut it is not protected
 });
 
 Route::prefix("/chatting")->group(function() {
@@ -78,16 +58,4 @@ Route::prefix("/convo")->group(function(){
 
     Route::delete('/delete', [ConversationController::class, "deleteConvo"]);
     
-});
-
-
-Route::prefix("/match")->group(function() {
-
-    Route::post("/request-match/{matchFrom}/{matchTo}", [MachingController::class, "requestMatching"]); // working well. but it is not protected
-
-    Route::put("/update-match/{matchFrom}/{matchTo}", [MachingController::class, "updateMatching"]); // working well. but it is not protected
-
-    Route::get("/list-match/{linkaUser}", [MachingController::class, "listMatching"]); // working well. but it is not protected
-
-    Route::delete("decline-match/{matchFrom}/{matchTo}", [MachingController::class, "declineMatching"]); // working well. but it is not protected
 });
