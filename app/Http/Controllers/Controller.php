27e26f4953cd39e5d9fdbcfcc3ad585defa5f33b
Jeph2001 <josephmaniragaba9@gmail.com>
2024-05-28 16:12:50 +0200

    small change

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,20 +6,6 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-/**
- * @OA\Info(
- *      title="Linka App Api Documentation",
- *      version="1.0.0",
- *      description="This Linka app will provide access to the database and coonsume data to your application",
- *      @OA\Contact(
- *          email="ganzatambaheritier@gmail.com",
- *      ),
- *      @OA\License(
- *          name="Apache 2.0",
- *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
- *      )
- * )
- */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
