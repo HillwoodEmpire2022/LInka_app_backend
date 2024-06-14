3f1fddb229be0bd1c309eca9ddf3fb7d1af90bc9
Jeph2001 <josephmaniragaba9@gmail.com>
2024-06-14 16:12:28 +0200

    improving therapy feature

--- app/Packages/Infrastructure/TherapistRepository.php
+++ app/Packages/Infrastructure/TherapistRepository.php
@@ -2,15 +2,18 @@
 
 namespace App\Packages\Infrastructure;
 use App\Models\Therapist;
+use App\Models\AppointmentVerification;
 
 
 class TherapistRepository{
 
     protected $therapistModels;
+    protected $verificationModels;
 
     public function __construct()
     {
         $this->therapistModels = new Therapist();
+        $this->verificationModels = new AppointmentVerification();
     }
 
     public function createTherapist(string $full_name, string $specialization, string $phone_number, ){
@@ -31,7 +34,12 @@ class TherapistRepository{
 
     public function getOneTherapist(int $id){
 
-        return $this->therapistModels->where('id', $id)->get();
+        $therapistInfo = $this->therapistModels->where('id', $id)->select('Full_names', 'Specialization', 'Phone_ontact')->first();
+        $appointmentData = $this->verificationModels->where('Therapist_Assigned', $id)->select('Full_name', 'Appointment_date', 'consultation_type', 'Appointment_Status')->get();
+        return response([
+            'Therapist Info'=>$therapistInfo,
+            'Appointment Info'=>$appointmentData,
+        ]);
 
     }
 
